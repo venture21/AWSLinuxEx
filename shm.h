c212abbef875b0709b106bc52dd94978d4266185
venture21 <park0122@gmail.com>
2021-07-06 17:01:24 +0900

    update stopWatch.c

--- shm.h
+++ shm.h
@@ -5,6 +5,7 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
-//공유메모리 공간 100 bytes
+//공유메모리 공간 100개 
+// sizeof(int)*SHMSIZE => 400 btyes
 #define SHMSIZE 100
-
+#define KeyValue 0x1234
--- stopWatch.c
+++ stopWatch.c
@@ -3,40 +3,27 @@
 #include <sys/time.h> // getimeofday( ) 함수에서 사용
 #include <stdlib.h>
 
-struct value
+#include <time.h>
+#include <stdio.h>
+
+
+int main(void)
 {
-	int start;
-	int end;
-};
+	int i;
+	long sum=0;
+	int startValue=1;
+	int endValue=2000000000;
+	
+	clock_t start_point, end_point;
+	// 시작 시간 기록
+	start_point = clock();
+	
+	for(i=startValue;i<=endValue;i++)
+	sum +=i;
+	
+	// 종료 시간 기록
+	end_point = clock();
 
-void disp_runtime(struct timeval UTCtime_s, struct timeval UTCtime_e)
-{
-	struct timeval UTCtime_r;
-	if((UTCtime_e.tv_usec- UTCtime_s.tv_usec)<0)
-	{
-		UTCtime_r.tv_sec  = UTCtime_e.tv_sec - UTCtime_s.tv_sec - 1;
-		UTCtime_r.tv_usec = 1000000 - UTCtime_e.tv_usec - UTCtime_s.tv_usec;
-	}
-	else
-	{
-		UTCtime_r.tv_sec = UTCtime_e.tv_sec - UTCtime_s.tv_sec;
-		UTCtime_r.tv_usec = UTCtime_e.tv_usec - UTCtime_s.tv_usec;
-	}
-	printf("runtime : %ld sec %ld\n", UTCtime_r.tv_sec, UTCtime_r.tv_usec);
-}
-
-int main()
-{
-    // linux micro sec까지 측정 
-    // UTCtime_s: 측정 시작시간
-    // UTCtime_e : 측정 종료시간
-    struct timeval UTCtime_s, UTCtime_e;
-
-    gettimeofday(&UTCtime_s, NULL);
-
-    // 100000usec -> 100msec
-    usleep(100000);
-
-    gettimeofday(&UTCtime_e, NULL);
-    disp_runtime(UTCtime_s, UTCtime_e);
+	printf("Exe time : %f sec\n", ((double)(end_point - start_point)/CLOCKS_PER_SEC));
+	printf("Result Value : %ld\n", sum); 
 }
