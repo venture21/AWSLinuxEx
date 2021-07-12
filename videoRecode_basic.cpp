57ec2336c13d0159b58eeabc8ba8fcb8be080089
venture21 <park0122@hanmdail.net>
2021-07-12 14:15:15 +0900

    update recode

--- videoRecode_basic.cpp
+++ videoRecode_basic.cpp
@@ -1,3 +1,6 @@
+// OpenCV를 사용하여 동영상 녹화하기
+// API설명 참조 사이트 : https://opencvlib.weebly.com/
+
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
@@ -9,46 +12,39 @@ using namespace std;
 
 int main(int, char**)
 {
-    //카메라 이미지 한 장을 저장하기 위한 이미지 객체 
-    Mat frame;
-
-    // 카메라를 선택하기 위한 객체
-    // cap은 fd와 동일한 존재
+    // 1. VideoCapture("동영상파일의 경로")
+    //    VideoCapture(0)
     VideoCapture cap;
-    // 카메라 갯수가 증가하면 0번부터 1씩 증가
     int deviceID = 0;
-    int apiID = cv::CAP_V4L2;   
+    int apiID = cv::CAP_V4L2;
 
+    Mat frame;
     // STEP 1. 카메라 장치 열기 
     cap.open(deviceID, apiID);
 
-    // check if we succeeded
     if (!cap.isOpened()) {
         perror("ERROR! Unable to open camera\n");
         return -1;
     }
+    //  라즈베리파이 카메라의 해상도를 1280X720으로 변경 
+    //cap.set(CAP_PROP_FRAME_WIDTH, 1280);
+    //cap.set(CAP_PROP_FRAME_HEIGHT, 720);
+    cap.set(CAP_PROP_FPS,60);
+    // Video Recording
+    //  현재 카메라에서 초당 몇 프레임으로 출력하고 있는가?
+    float videoFPS = cap.get(CAP_PROP_FPS);
+    int videoWidth = cap.get(CAP_PROP_FRAME_WIDTH);
+    int videoHeight = cap.get(CAP_PROP_FRAME_HEIGHT);
 
-    //--- GRAB AND WRITE LOOP
-    printf("Start grabbing\n"); 
-    printf("Press any key to terminate\n");
+    printf("videoFPS=%f\n",videoFPS);
+    printf("width=%d, height=%d\n",videoWidth, videoHeight);
 
-    while(1)
-    {
-        // 카메라에서 매 프레임마다 이미지 읽기
-        cap.read(frame);
+    // while(1)
+    // {
 
-        // check if we succeeded
-        if (frame.empty()) {
-            perror("ERROR! blank frame grabbed\n");
-            break;
-        }
-        // "Live"라는 창을 생성하고, 그 창에 frame 이미지를 보여준다.
-        imshow("Live", frame);
-	// waitKey함수는 ms단위로 대기함수 
-	// ()가 비어 있으면 키입력이 들어오기 전까지 무한 대기
-        if (waitKey(5) >= 0)
-            break;
-    }
-    // the camera will be deinitialized automatically in VideoCapture destructor
+    // }
+
+
+
     return 0;
 }
